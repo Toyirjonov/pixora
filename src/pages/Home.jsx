35f040ead3aaa259774e23b6450276486af2cbd9
Toyirjonov <shoxruxmirzo12345@mail.ru>
2025-08-04 15:59:12 +0500

    add collection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { Fragment } from "react";
+import { useCollection } from "../hooks/useCollection";
+
 function Home() {
+  const { data: users } = useCollection("users");
+
+  if (!users) {
+    return <h2 className="text-3xl">Loading...</h2>;
+  }
   return (
     <section className=" w-full flex items-center justify-center h-full">
       <h1 className="text-7xl  text-black">Welcome to Home</h1>
+      <br />
+      {users &&
+        users.map((user) => {
+          return (
+            <Fragment key={user.id}>
+              {user.online && (
+                <div className="avatar avatar-online">
+                  <div className="w-24 rounded-full">
+                    <img src={user.photoURL} />
+                  </div>
+                </div>
+              )}
+              {!user.online && (
+                <div className="avatar avatar-offline">
+                  <div className="w-24 rounded-full"></div>
+                </div>
+              )}
+            </Fragment>
+          );
+        })}
     </section>
   );
 }
